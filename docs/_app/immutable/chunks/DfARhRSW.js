f97234b53c4a85b53e5941858c9c55ee4e3d7a8b
Tania Elisa Celis Gamboa <tancel@elevad.umea.se>
2025-05-07 14:25:15 +0200

    devblog2

---
+++ docs/_app/immutable/chunks/DfARhRSW.js
@@ -0,0 +1,9 @@
+import{s as p,n as r}from"./COSaXkqD.js";import{S as y,i as w,d as o,a as s,c,g as I,b as _,f as v,e as m,s as g,t as b}from"./C7rjZEja.js";function x(f){let e,u="Hello!",l,n,i,d,h;return{c(){e=m("h1"),e.textContent=u,l=g(),n=m("br"),i=b(`
+Today I added a navigation bar, and made it only visible when hovering over it with a mouse and it stays at the top of the screen all the time.
+I also added some placeholders for the icons which will send you to my socials (I'll add that another day).
+`),d=m("br"),h=b(`
+I want the images in sketches to loop around as you scroll, I have not been able to figure out how to do that.`)},l(t){e=c(t,"H1",{"data-svelte-h":!0}),I(e)!=="svelte-gbi83v"&&(e.textContent=u),l=_(t),n=c(t,"BR",{}),i=v(t,`
+Today I added a navigation bar, and made it only visible when hovering over it with a mouse and it stays at the top of the screen all the time.
+I also added some placeholders for the icons which will send you to my socials (I'll add that another day).
+`),d=c(t,"BR",{}),h=v(t,`
+I want the images in sketches to loop around as you scroll, I have not been able to figure out how to do that.`)},m(t,a){s(t,e,a),s(t,l,a),s(t,n,a),s(t,i,a),s(t,d,a),s(t,h,a)},p:r,i:r,o:r,d(t){t&&(o(e),o(l),o(n),o(i),o(d),o(h))}}}const C={title:"Devblog 2",date:"2025-05-07"},{title:B,date:H}=C;class R extends y{constructor(e){super(),w(this,e,null,x,p,{})}}export{R as default,C as metadata};
---
+++ docs/_app/immutable/entry/start.CMTYhtZv.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/BGmLE5k4.js";export{o as load_css,r as start};
