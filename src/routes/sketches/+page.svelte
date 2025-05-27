2a44626dafa43b9fe7f06725b6aa3bd01a2d947f
Tania Elisa Celis Gamboa <tancel@elevad.umea.se>
2025-05-27 18:55:38 +0200

    message

--- src/routes/sketches/+page.svelte
+++ src/routes/sketches/+page.svelte
@@ -1,34 +1,88 @@
 <script>
-    import Carousel from 'svelte-carousel'
+    import { onMount } from "svelte";
 
-    let artworks = [{img:"IMG_5216.jpg"}, {img:"IMG_5308.jpeg"},{img:"sketch1.jpeg"},{img:"sketch2.jpeg"},{img:"sketch3.jpeg"},{img:"sketch4.jpeg"},{img:"sketch5.jpeg"}]
+    let skeches = [{img:"IMG_5216.jpg"}, {img:"IMG_5308.jpeg"},{img:"sketch1.jpeg"},{img:"sketch2.jpeg"},{img:"sketch3.jpeg"},{img:"sketch4.jpeg"},{img:"sketch5.jpeg"}]
+    let moreSkeches = [...skeches]
+
+    let things;
+
+    onMount(() => {
+        things = document.querySelectorAll(".thing");
+        const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("show", entry.isIntersecting);
+        });
+        console.log(entries)
+    },
+    {
+        threshold: 0.9,
+    }
+    );
+
+    things.forEach(thing => {
+        observer.observe(thing);
+    });
+    
+    return () => {
+        if (things) {
+            things.forEach(thing => {
+                observer.unobserve(thing);
+            });
+        }
+    }
+})
 </script>
 
 <main>
-    <Carousel
+    <div class="horizontal-scroll">
+        {#each moreSkeches as skech}
         
-        particlesToShow={2}
-        particlesToScroll={1}
-    >
-    
-        {#each artworks as art}
-        
-            <img src={art.img} alt="i" id="artwork">
+            <img src={skech.img} alt="i" id="sketches" class="thing show">
 
         {/each}
-        
-    </Carousel>
+    </div>
+    <div class="white"></div>
 </main>
 
 <style>
     main{
         overflow-y: hidden;
-        background-color: aliceblue;
+        height: 100vh;
+        background-color: rgb(255, 215, 240);
+        padding-top: 8vh;
     }
-   #artwork{
-    height: 100vh;
-    object-fit: contain;
-    padding: 5vh;
-}
-    
+    .horizontal-scroll{
+        overflow-y: hidden;
+        overflow-x: auto;
+        height: 80vh;
+        width: fit-content;
+        display: flex;
+        flex-wrap: nowrap;
+        align-items: center;
+        scroll-behavior: smooth;
+        
+    }
+    #sketches{
+            flex: 0 0 auto;
+            margin: 100px;
+            vertical-align: middle;
+            background-color: rgb(0, 0, 0);
+            padding: 3vh;
+            object-fit: contain;
+        }
+    .thing{
+        transition: 600ms ease-in-out;
+        width: 50vw;
+        height: 60vh;
+    }
+    .thing.show{
+        width: 60vw;
+        height: 70vh;
+    }
+    .white{
+		background-color: white;
+		height: 14vh;
+        width: 100%;
+        position: fixed;
+	}
 </style>
